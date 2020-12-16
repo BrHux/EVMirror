bd5c22542c4cf38bf7de3fcad23bdeadf7f0a350
BrHux <278488068@163.com>
2020-12-16 14:43:21 +0800

    [功能]添加投屏界面

---
+++ app/src/main/java/cn/ieway/evmirror/modules/screenshare/ScreenShareActivity.java
@@ -0,0 +1,27 @@
+package cn.ieway.evmirror.modules.screenshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.ieway.evmirror.R;
+import cn.ieway.evmirror.base.BaseActivity;
+
+public class ScreenShareActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_share);
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
